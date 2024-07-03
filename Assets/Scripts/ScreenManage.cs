c996effc1aaf1d4fbd82b486d2d8cadda907765a
Oğuzhan Yılmaz <oguzhan06_61@hotmail.com>
2024-07-04 01:20:11 +0300

    UI-1
    First commit

---
+++ Assets/Scripts/ScreenManage.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScreenManage : MonoBehaviour
+{
+    public void playGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+    public void Restart()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void MainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
